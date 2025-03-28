f421be049232f184eaea3ec33c53b284b8782a5f
Miami Crypto Labs <105951495+MiamiCrypto@users.noreply.github.com>
2025-03-28 15:45:02 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
+import azure.functions as func
 
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    return func.HttpResponse(f"Hello, {name}!", status_code=200)
